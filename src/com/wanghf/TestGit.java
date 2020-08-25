856988019c5512df5a91ef79e5ab2345e8c1c289
Administrator <Administrator@ZJZL-20200628XP>
2020-08-25 11:02:56 +0800

    增加一行代码

--- src/com/wanghf/TestGit.java
+++ src/com/wanghf/TestGit.java
@@ -3,6 +3,7 @@ package com.wanghf;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("hello, git");
+		System.out.println("hello, git2");
 	}
 
 }
---
+++ src/com/wanghf/pojo/Student.java
@@ -0,0 +1,5 @@
+package com.wanghf.pojo;
+
+public class Student {
+
+}
