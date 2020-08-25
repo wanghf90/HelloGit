6a5f73ea071fde9eea773993f933763778f3c5d9
Administrator <Administrator@ZJZL-20200628XP>
2020-08-25 11:14:40 +0800

    2020-08-25优化代码

--- src/com/wanghf/TestGit.java
+++ src/com/wanghf/TestGit.java
@@ -4,6 +4,8 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("hello, git");
 		System.out.println("hello, git2");
+		System.out.println("hello, git3");
+		System.out.println("hello, git4");
 	}
 
 }
